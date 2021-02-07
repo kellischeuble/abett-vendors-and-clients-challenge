7516993835279878c5945ed76bfd3e0a2aadb0ee
Kelli Scheuble <github@scheuble.dev>
2021-02-06 20:43:26 -0800

    Outlines functions

--- src/main.py
+++ src/main.py
@@ -1,3 +1,27 @@
 import sqlite3
 
-conn = sqlite3.connect("vendors.db")
+conn = sqlite3.connect(":memory")
+
+def insert_vendor(vend):
+    pass
+
+def getvendor(vend):
+    pass
+
+def update_vendor(vend):
+    pass
+
+def remove_client(vend):
+    pass
+
+def insert_client(vend):
+    pass
+
+def getclient(vend):
+    pass
+
+def update_client(vend):
+    pass
+
+def remove_client(vend):
+    pass
